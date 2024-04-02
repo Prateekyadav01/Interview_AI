37d25ff2656fbc3c6c0ed121becc91617bf2d2e1
PrateekTechmatys <prateeky562@gmail.com>
2024-04-02 09:06:16 +0530

    download is done

--- src/index.js
+++ src/index.js
@@ -1,28 +1,73 @@
-const url = 'https://linkedin-jobs-scraper-api.p.rapidapi.com/jobs';
+// const url = 'https://linkedin-jobs-scraper-api.p.rapidapi.com/jobs';
+// const options = {
+// 	method: 'POST',
+// 	headers: {
+// 		'content-type': 'application/json',
+// 		'X-RapidAPI-Key': '5fb769ce55msh7a0078af2e4d4b6p107e23jsn93d400fe3a10',
+// 		'X-RapidAPI-Host': 'linkedin-jobs-scraper-api.p.rapidapi.com'
+// 	},
+// 	body: {
+// 		title: 'Software Engineer',
+// 		location: 'Berlin',
+// 		rows: 100
+// 	}
+// };
+
+
+
+// async function all(){
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.text();
+//         console.log(result);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+// all();
+
+
+// const url = 'https://remoote-job-search1.p.rapidapi.com/remoote/skills';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '5fb769ce55msh7a0078af2e4d4b6p107e23jsn93d400fe3a10',
+// 		'X-RapidAPI-Host': 'remoote-job-search1.p.rapidapi.com'
+// 	}
+// };
+
+// async function a(){
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.text();
+//         console.log(result);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+// a();
+
+
+const url = 'https://jsearch.p.rapidapi.com/search?query=Python%20developer%20in%20Texas%2C%20USA&page=1&num_pages=1';
 const options = {
-	method: 'POST',
+	method: 'GET',
 	headers: {
-		'content-type': 'application/json',
 		'X-RapidAPI-Key': '5fb769ce55msh7a0078af2e4d4b6p107e23jsn93d400fe3a10',
-		'X-RapidAPI-Host': 'linkedin-jobs-scraper-api.p.rapidapi.com'
-	},
-	body: {
-		title: 'Software Engineer',
-		location: 'Berlin',
-		rows: 100
+		'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
 	}
 };
 
 
-
-async function all(){
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-    }
+async function a(){
+	try {
+		const response = await fetch(url, options);
+		const result = await response.json();
+		console.log(result);
+	} catch (error) {
+		console.error(error);
+	}
 }
 
-all();
+a();
